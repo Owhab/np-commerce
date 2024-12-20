1872bebe7dab102ccf3c259faabeaf7106d2f351
Abdul Owhab <owhab.cse@gmail.com>
2024-12-20 16:23:16 +0600

    project initialization

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express, {Express, Request, Response} from 'express';
+
+const app: Express = express();
+const port: number = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World');
+});
+
+app.listen(port, () => {
+    console.log(`Server started at http://localhost:${port}`);
+});
+
+
